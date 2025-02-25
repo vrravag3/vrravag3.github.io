afd226c05d4ffb2d6dc5773080b05a283caf607d
vrravag3 <vrravag3@cougarnet.uh.edu>
2025-02-25 00:26:01 -0600

    Update homework2.js

--- mis3371/homework2/homework2.js
+++ mis3371/homework2/homework2.js
@@ -11,16 +11,24 @@ function reviewInput()
 {
     var formcontent = document.getElementById("psignup");
     var formoutput = "<table class='output'><caption>Review Your Information</caption>";
-    for (let i = 0; i < formcontent.length; i++) {
-        if (formcontent.elements[i].value !== "") {
-            switch (formcontent.elements[i].type) {
+    for (let i = 0; i < formcontent.length; i++) 
+    {
+                if (fieldType === "button" || fieldType === "submit" || fieldType === "reset") {
+            continue;
+        }
+        if (formcontent.elements[i].value !== "") 
+        {
+            switch (formcontent.elements[i].type) 
+            {
                 case "checkbox":
-                    if (formcontent.elements[i].checked) {
+                    if (formcontent.elements[i].checked) 
+                    {
                         formoutput += `<tr><td align='right'>${formcontent.elements[i].name}</td><td>&#x2713;</td></tr>`;
                     }
                     break;
                 case "radio":
-                    if (formcontent.elements[i].checked) {
+                    if (formcontent.elements[i].checked) 
+                    {
                         formoutput += `<tr><td align='right'>${formcontent.elements[i].name}</td><td>${formcontent.elements[i].value}</td></tr>`;
                     }
                     break;
