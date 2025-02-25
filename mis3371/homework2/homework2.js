14fd4b49d83f7e53f8cada63a9f1d0597f3106b1
vrravag3 <vrravag3@cougarnet.uh.edu>
2025-02-24 21:45:34 -0600

    Update homework2.js

--- mis3371/homework2/homework2.js
+++ mis3371/homework2/homework2.js
@@ -10,7 +10,7 @@ Purpose: A Clinic Patient form
 function reviewInput() 
 {
     var formcontent = document.getElementById("psignup");
-    var formoutput = "<table class='output'><th><center> Review Your Information:</center></th>";
+    var formoutput = "<table class='output'><th>Review Your Information:</th>";
     for (let i = 0; i < formcontent.length; i++) {
         if (formcontent.elements[i].value !== "") {
             switch (formcontent.elements[i].type) {
